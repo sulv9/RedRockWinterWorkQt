212ab4292f89fab6ec356683625fbcb2209e2bc7
aefottt <aefottt@outlook.com>
2021-01-23 01:10:50 +0800

    更新主界面

---
+++ app/src/main/java/com/aefottt/redrockwinterworkqt/CircleImageView.java
@@ -0,0 +1,104 @@
+package com.aefottt.redrockwinterworkqt;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * 通过BitmapShader着色器来绘制圆形图片
+ */
+public class CircleImageView extends androidx.appcompat.widget.AppCompatImageView {
+    private Paint mPaint; //画笔
+    private Matrix mMatrix; //矩阵
+    private int mRadius; //半径
+    private Bitmap mBitmap; //Bitmap图片
+    private BitmapShader mBitmapShader; //着色器
+
+    public CircleImageView(@NonNull Context context) {
+        super(context);
+    }
+
+    public CircleImageView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CircleImageView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * 初始化操作
+     */
+    private void init() {
+        // 判断是否有图片资源
+        if (getDrawable() == null){
+            return;
+        }
+        mPaint = new Paint();
+        mMatrix = new Matrix();
+        mBitmap = drawableToBitmap(getDrawable());
+        mBitmapShader = new BitmapShader(mBitmap, Shader.TileMode.CLAMP,
+                Shader.TileMode.CLAMP);
+    }
+
+    /**
+     * 将Drawbable转化为Bitmap
+     */
+    private Bitmap drawableToBitmap(Drawable drawable) {
+        // 是否为BitmapDrawable的子类
+        if (drawable instanceof BitmapDrawable){
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+        // 通过画布将drawable画到bitmap上
+        int width = drawable.getIntrinsicWidth();
+        int height = drawable.getIntrinsicHeight();
+        //创建一个宽高与图片一样长的Bitmap
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        //创建以bitmap为背景的画布
+        Canvas canvas = new Canvas(bitmap);
+        //将drawable画到canvas上
+        drawable.setBounds(0, 0, width, height);
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //获取最短的边设为半径
+        mRadius = Math.min(getMeasuredWidth(), getMeasuredHeight()) / 2;
+
+        //重新设置图片大小
+        setMeasuredDimension(mRadius * 2, mRadius * 2);
+    }
+
+    /**
+     * 进行绘制
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+//        super.onDraw(canvas); TODO 这里一定要删掉才能绘制成功！
+        // 计算缩放比例
+        float mScale =(mRadius * 2.0f) / Math.min(mBitmap.getWidth(), mBitmap.getHeight());
+        // 设置矩阵的偏移度
+        mMatrix.setScale(mScale, mScale);
+        // 配备矩阵给着色器
+        mBitmapShader.setLocalMatrix(mMatrix);
+        // 配备着色器给画笔
+        mPaint.setShader(mBitmapShader);
+        // 在画布上画圆
+        canvas.drawCircle(mRadius, mRadius, mRadius, mPaint);
+    }
+}
--- app/src/main/java/com/aefottt/redrockwinterworkqt/MainActivity.java
+++ app/src/main/java/com/aefottt/redrockwinterworkqt/MainActivity.java
@@ -1,14 +1,93 @@
+
 package com.aefottt.redrockwinterworkqt;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.aefottt.redrockwinterworkqt.adapter.NormalVpAdapter;
+import com.aefottt.redrockwinterworkqt.fragment.IndexFragment;
+import com.aefottt.redrockwinterworkqt.fragment.NaviFragment;
+import com.aefottt.redrockwinterworkqt.fragment.ProjectFragment;
+import com.aefottt.redrockwinterworkqt.fragment.TreeFragment;
+import com.aefottt.redrockwinterworkqt.fragment.UserArticleFragment;
+import com.aefottt.redrockwinterworkqt.fragment.WendaFragment;
+import com.aefottt.redrockwinterworkqt.fragment.WxarticleFragment;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+
+    private EditText etMain;
+    private CircleImageView accountMain;
+    private TabLayout tbMain;
+    private ViewPager vpMain;
+
+    private static final String[] titles = { "导航", "首页", "广场", "问答", "项目", "体系", "公众号" };
+    private ArrayList<Fragment> fList;
+    private NormalVpAdapter vpAdapterMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // 5.0以上修改状态栏字体颜色
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+
+        initView();
+        initData();
+
+        vpMain.setAdapter(vpAdapterMain);
+        vpMain.setCurrentItem(1);
+        tbMain.setupWithViewPager(vpMain);
+    }
+
+    @Override
+    protected void initData() {
+        //添加Tab
+        for (String title : titles) {
+            tbMain.addTab(tbMain.newTab().setText(title));
+        }
+        //添加Fragment
+        fList = new ArrayList<>();
+        fList.add(new NaviFragment());
+        fList.add(new IndexFragment());
+        fList.add(new UserArticleFragment());
+        fList.add(new WendaFragment());
+        fList.add(new ProjectFragment());
+        fList.add(new TreeFragment());
+        fList.add(new WxarticleFragment());
+        //初始化VpAdapter
+        vpAdapterMain = new NormalVpAdapter(getSupportFragmentManager(),
+                FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT,
+                fList, titles);
+    }
+
+    @Override
+    protected void initView() {
+        etMain = findViewById(R.id.et_main_search);
+        etMain.setOnClickListener(this);
+        accountMain = findViewById(R.id.iv_main_account);
+        accountMain.setOnClickListener(this);
+        tbMain = findViewById(R.id.tb_main);
+        vpMain = findViewById(R.id.vp_main);
+    }
+
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.iv_main_account){
+            //TODO 点击Account按钮前往用户界面（希望有MaterialAnimation）
+        }else if (id == R.id.et_main_search){
+            //TODO 点击搜索框前往搜索界面
+        }
     }
 }
